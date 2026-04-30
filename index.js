076851578306a91b0cd4e73b70a8d362a98de2e0
Moha <mohammedelsadig111@gmail.com>
2026-05-01 00:49:07 +0400

    Everything

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let name;
-
 process.stdout.write("What's your name? ");
 process.stdin.on("data", (d) => {
+    process.stdout.write("صباح الكيك على عينيك");
     console.log(d.toString().trim());
     process.exit(0);
 });

27fb35262e1fe9d404cf6af690e0fe286d523992
Moha <mohammedelsadig111@gmail.com>
2026-05-01 15:16:07 +0400

    Updated Branch

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-const Client = document.getElementById("client");
+let name;
 
-function distance(p1, p2){
-    return Math.sqrt(
-        Math.pow(p2.x - p1.x, 2) +
-        Math.pow(p2.y - p1.y, 2)
-    );
-}
-
-let last_coords = {x: 0, y: 0};
-let last_time = Date.now();
-
-document.addEventListener("mousemove", e => {
-    let current_coords = {
-        x: e.clientX,
-        y: e.clientY
-    };
-
-    let current_time = Date.now();
-
-    let d = distance(last_coords, current_coords);
-    let t = current_time - last_time;
-
-    if (t === 0) return;
-
-    let V = d / (t / 1000); // px per second
-
-    Client.textContent = `Speed: ${V.toFixed(2)} px/s`;
-
-    last_coords = current_coords;
-    last_time = current_time;
+process.stdout.write("What's your name? ");
+process.stdin.on("data", data => {
+	name = data.toString().trim();
+	process.stdout.write("Nice to meet you, %s!", name);
 });

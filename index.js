38462b357c88b9c5effff728dce2cace46314157
pbzv <mohammedelsadig111@gmail.com>
2026-05-02 21:40:23 +0400

    Header1

--- index.js
+++
@@ -1,7 +0,0 @@
-let name;
-
-process.stdout.write("What's your name? ");
-process.stdin.on("data", data => {
-	name = data.toString().trim();
-	process.stdout.write("Nice to meet you, %s!", name);
-});

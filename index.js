b0925a6888d1994a56f13991bb29cb35c0813966
pbzv <mohammedelsadig111@gmail.com>
2026-05-12 21:31:04 +0400

    Moha edit v1

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const head = document.querySelector('header');
 window.addEventListener('scroll', () => {
     if(window.scrollY > 60) {
         head.classList.add('scrolled');
-    }
-    else {
+    } else {
         head.classList.remove('scrolled');
+    }
+});
+
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('change', () => {
+    if(toggle.checked) {
+        console.log("Switch is ON")
+    } else {
+        console.log("Switch is OFF")
     }
 });

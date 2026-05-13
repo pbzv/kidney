2e9c95de561536907321f895001f6a4d8dbbf838
pbzv <mohammedelsadig111@gmail.com>
2026-05-13 13:19:24 +0400

    Moha edit v2

--- index.js
+++ index.js
@@ -10,9 +10,25 @@ window.addEventListener('scroll', () => {
 
 const toggle = document.getElementById('toggle');
 
+
+
 toggle.addEventListener('change', () => {
     if(toggle.checked) {
-        console.log("Switch is ON")
+        Notification.requestPermission().then(permission => {
+            if(permission === "granted"){
+                const notify = new Notification("Kidney App",
+                    {
+                        body: "Don't forget to drink water and stay hydrated!",
+                        icon: "logo.png",
+                        requireInteraction: true
+                    }
+                )
+                notify.onclick = () => {
+                    window.focus();
+                    notify.close();
+                }
+            }
+        })
     } else {
         console.log("Switch is OFF")
     }
